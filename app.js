938129f5bc6dc06541990db3d430785a62579b4d
mkiterian <hmukite@gmail.com>
2019-01-18 03:15:03 +0300

    set up cors

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("./config/config");
 const mongoose = require("./db/mongoose");
 const router = require("./routes");
 
 const app = express();
 app.use(bodyParser.json());
+const corsOptions = {
+  exposedHeaders: 'x-auth',
+};
+app.use(cors(corsOptions));
 
 app.use("/", router);
 
